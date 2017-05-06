677eeabd47bc482f65b15f801e98fc490860d9ce
Dax Ravi <d@ironbay.digital>
2017-05-06 11:35:33 -0400

    Text

---
+++ src/components/text/index.tsx
@@ -0,0 +1,16 @@
+import './styles.css'
+import * as React from 'react'
+import wrap from '../wrap'
+
+import Container from '../container'
+
+export default wrap(Container, 'text', {}, [
+	'xxs',
+	'xs',
+	's',
+	'l',
+	'xl',
+	'xxl',
+
+	'stretch'
+])
--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -2,6 +2,7 @@ import './styles.css'
 import * as React from 'react'
 
 import Container from '../../components/container'
+import Text from '../../components/text'
 
 export default class HomePage extends React.Component<any, any> {
 	constructor() {
@@ -9,8 +10,14 @@ export default class HomePage extends React.Component<any, any> {
 	}
 	render() {
 		return (
-			<Container pad-l className='home-page'>
-				Hello this is the home page!
+			<Container vertical pad-l className='home-page'>
+				<Text xxs>Text xxs</Text>
+				<Text xs>Text xs</Text>
+				<Text s>Text s</Text>
+				<Text>Text</Text>
+				<Text l>Text l</Text>
+				<Text xl>Text xl</Text>
+				<Text xxl>Text xxl</Text>
 			</Container>
 		)
 	}
