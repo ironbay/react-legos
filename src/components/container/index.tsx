6bb18a4dd72df39ed0c41a4ba9ca371ae916973f
Dax Ravi <d@ironbay.digital>
2017-05-05 21:19:20 -0400

    Container progress

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -5,4 +5,21 @@ import wrap from '../wrap'
 export default wrap('div', 'container', {}, [
 	'horizontal',
 	'vertical',
+
+	'align-center',
+	'justify-center',
+
+	'margin-v',
+	'margin-h',
+	'margin-right',
+	'margin-left',
+	'margin-top',
+	'margin-bottom',
+
+	'pad-v',
+	'pad-h',
+	'pad-right',
+	'pad-left',
+	'pad-top',
+	'pad-bottom',
 ])
